a2981e2eea82e95074f866d30000aed2da6cee7d
Pim Snel <post@pimsnel.com>
2024-12-12 20:42:06 +0100

    last stuff for first version

--- RUNME.sh
+++ RUNME.sh
@@ -48,27 +48,50 @@ function checkOS {
   fi
 }
 
+function checkBlockDevices {
+  if [ -f /etc/lsb-release ]; then
+    osType="linux"
+    lsblk -fs
+  elif [ -f /System/Library/CoreServices/SystemVersion.plist ]; then
+    osType="macos"
+    diskutil list
+  else
+    osType="other"
+  fi
+}
+
+
+
 make_command "audit" "Run Audit Tool to create report"
 audit(){
 
  show_version
 
- checkdeps "jq"
+ #checkdeps "jq"
  checkdeps "lynis"
  checkdeps "docker"
- exit
+ checkdeps "sed"
+ checkdeps "neofetch"
+ checkdeps "tar"
 
+ output=output-$(whoami)-$(date +"%d-%m-%Y")
+ tarball=honeybadger-$(whoami)-$(date +"%d-%m-%Y").tar.bz2
+ mkdir -p $output
+ docker rm lynis_converter 2> /dev/null
  sudo lynis audit system
  docker build -t kuznetsovv/lynis-report-converter .
  docker run -d --name lynis_converter kuznetsovv/lynis-report-converter
  sudo cp /var/log/lynis-report.dat /tmp
  sudo chmod 666 /tmp/lynis-report.dat
- #docker ps
  docker cp /tmp/lynis-report.dat lynis_converter:/var/log/lynis-report.dat
- docker exec lynis_converter ./opt/lynis-report-converter-master/lynis-report-converter.pl -j >> lynis-report-$(whoami)-$(date +"%d-%m-%Y").json
+ docker exec lynis_converter ./opt/lynis-report-converter-master/lynis-report-converter.pl -j > $output/lynis-report.json
  docker stop lynis_converter
  docker rm lynis_converter
  sudo rm /tmp/lynis-report.dat
+ neofetch|sed 's/\x1B\[[0-9;]*m//g' > $output/neofetch.txt
+ show_version > $output/honeybadger-info.txt
+ checkBlockDevices > $output/blockdevices.txt
+ tar cjvf $tarball $output
 }
 
 ##### PLACE YOUR COMMANDS ABOVE #####
