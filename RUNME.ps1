90f973c0705855b457bb22319fdd85477d11b3fe
Wouter van der Toorren <wouter@technative.eu>
2025-10-07 11:17:39 +0200

    Update RUNME.ps1
    fix error

--- RUNME.ps1
+++ RUNME.ps1
@@ -2,14 +2,16 @@ echo "NEW REPORT" > audit-report.txt
 
 mkdir report
 $bitlocker_outfile = ".\report\bitlocker_result.txt"
-$code = "Get-BitLockerVolume 2>&1 >>$bitlocker_outfile"
+$code = "Get-BitLockerVolume 2>&1 | Out-File -FilePath `"$bitlocker_outfile`" -Encoding utf8 -Append"
 
 $prog = "powershell.exe"
-Invoke-Command  -ScriptBlock { param($p,$a,$o)
-                                                            Start-Process $p -ArgumentList $a -wait -verb RunAs
-                                                            get-content $o
-                                                        } -ArgumentList $prog,$code,$output
+
+# Pass the code as argument for -Command, en geef het outputpad mee als $o
+Invoke-Command -ScriptBlock { param($p,$a,$o)
+    # Start elevated PowerShell die de expressie ($a) uitvoert
+    Start-Process $p -ArgumentList @('-NoProfile','-Command',$a) -Wait -Verb RunAs
+    Get-Content $o
+} -ArgumentList $prog,$code,$bitlocker_outfile
 
 Import-Module .\HardeningKitty\HardeningKitty.psm1
-
 Invoke-HardeningKitty -Mode Config -Report -ReportFile .\report\hardeningkitty.csv
